6ddd060794028dd4aec96ea0ee15db11307acea4
David Carpenter <carpenterd777@gmail.com>
2022-06-11 10:33:58 -0400

    separate routes for tokens and cards

--- src/pages/card/[id]/[name].tsx
+++ src/pages/card/[id]/[name].tsx
@@ -1,7 +1,7 @@
-import { GetStaticPaths } from "next";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import Database from "../../utils/database";
+import Database from "../../../utils/database";
 
 const CardPage = (): JSX.Element => {
   const router = useRouter();
@@ -20,19 +20,30 @@ const CardPage = (): JSX.Element => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const db = new Database();
-  const numCards = await db.card_count();
 
-  const allNames: Array<string> = [];
+  const numCards = await db.nontoken_count();
+  const paths: Array<{ params: { id: string; name: string } }> = [];
+
   // for each card, get it's name and add that to an array
-  for (const i of [...Array(numCards).keys()]) {
+  for (const i of [...Array(numCards - 1).keys()]) {
     const id = `${i + 1}`;
     const name = await db.route_name(id);
-    allNames.push(name);
+    paths.push({ params: { id: id, name: name } });
   }
 
   return {
-    paths: allNames,
+    paths: paths,
     fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = ({
+  params,
+}: GetStaticPropsContext) => {
+  if (params === undefined) throw new Error("Could not get params!");
+  const name = params.name;
+  return {
+    props: { name },
   };
 };
 
---
+++ src/pages/token/[id]/[name].tsx
@@ -0,0 +1,50 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Database from "../../../utils/database";
+
+const CardPage = (): JSX.Element => {
+  const router = useRouter();
+  const { name } = router.query;
+
+  return (
+    <>
+      <Head>
+        <title>{name} - Rise to Ragnarök - Bindafall</title>
+        <meta name="description" content="Preview of custom cards" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const db = new Database();
+
+  const numTokens = await db.token_count();
+  const paths: Array<{ params: { id: string; name: string } }> = [];
+
+  // for each token, get its route name and add it to the list
+  for (const i of [...Array(numTokens).keys()]) {
+    const id = `${i + 1}`;
+    const name = await db.route_name(id, true);
+    paths.push({ params: { id: id, name: name } });
+  }
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = ({
+  params,
+}: GetStaticPropsContext) => {
+  if (params === undefined) throw new Error("Could not get params!");
+  const name = params.name;
+  return {
+    props: { name },
+  };
+};
+
+export default CardPage;

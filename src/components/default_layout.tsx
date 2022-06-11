a4502b3ceb3df514d793c556f79b6dae0904efe9
David Carpenter <carpenterd777@gmail.com>
2022-06-11 11:14:32 -0400

    default layout accepts children

---
+++ src/components/default_layout.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from "react";
+
+const DefaultLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <main>
+      <div className="mx-36 my-6 text-center">{children}</div>
+    </main>
+  );
+};
+
+export default DefaultLayout;

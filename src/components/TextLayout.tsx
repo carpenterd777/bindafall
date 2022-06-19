d96c93f432892861e89aaa69c6c57b68168a78c3
David Carpenter <carpenterd777@gmail.com>
2022-06-18 23:08:34 -0400

    fix footer rendering in the middle of page

--- src/components/TextLayout.tsx
+++ src/components/TextLayout.tsx
@@ -3,7 +3,7 @@ import { FC, ReactNode } from "react";
 const TextLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <main>
-      <div className="mx-4 lg:mx-72 my-6 text-left h-[100vh]">{children}</div>
+      <div className="mx-4 lg:mx-72 my-6 text-left h-auto">{children}</div>
     </main>
   );
 };
